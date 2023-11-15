93c305db7e78a75b7490d99312b8d65010ffa3ba
SnippyRex <kentf022@gmail.com>
2023-11-15 01:06:02 +0000

    Create Article Card

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -7,13 +7,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -25,12 +29,18 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.newsapp.ui.theme.NewsAppTheme
 import com.example.newsapp.viewmodel.NewsViewModel
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,6 +78,38 @@ fun NewsApp(newsViewModel: NewsViewModel = viewModel()) {
     }
 }
 
+@Composable
+fun ArticleCard(claim: String, summary: String) {
+    Card(modifier = Modifier.shadow(4.dp, RoundedCornerShape(32.dp))) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = claim,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.size(4.dp))
+            Divider()
+            Spacer(modifier = Modifier.size(4.dp))
+            Text(
+                text = summary,
+                fontWeight = FontWeight.Light,
+                maxLines = 3,
+                overflow = TextOverflow.Clip,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ArticleCardExample() {
+    ArticleCard(
+        claim = "Air Traffic Control Staff Shortages Cause Flight Disruptions at London's Gatwick Airport",
+        summary = "Thousands of passengers face flight disruptions at London's Gatwick Airport due to air traffic control staff shortages, causing delays, diversions, and cancellations."
+    )
+}
 
 @Composable
 fun NewsAppTopBar() {
